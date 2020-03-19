421598e29b0c92699fea4e5ace28920610c088bd
Kjell Wooding <kjell@wooding.org>
2020-03-19 13:56:23 -0400

    initial generation from EasyData

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='''This dataset is intended to mobilize researchers to apply recent advances in natural language processing to generate new insights in support of the fight against COVID-19.''',
+    author='Tutte Institute',
+    license='MIT',
+)
